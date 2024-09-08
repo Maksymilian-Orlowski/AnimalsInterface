2b26fdc4e2bc9e4bafb07026f8b30d107ec620f7
Maksymilian-Orlowski <orlowski.maksymilian@op.pl>
2024-09-08 14:07:55 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.ReadKey();
+    }
+
+
+    interface AnimalAction
+    {
+        void Eat();
+        void Drink();
+        void Walk();
+        void Jump();
+
+    }
+
+    class Cat : AnimalAction 
+    {
+        public void Eat()
+        {
+            Console.WriteLine("*deliciois!*");
+        }
+
+        public void Drink()
+        {
+            Console.WriteLine("*plum-plum*");
+        }
+
+        public void Walk()
+        {
+        }
+
+        public void Jump()
+        {
+        }
+
+        public void Meow()
+        {
+            Console.WriteLine("Meow! Meow!");
+        }
+
+    }
+
+    class Dog
+    {
+        public void Eat()
+        {
+        }
+
+        public void Drink()
+        {
+
+        }
+
+        public void Walk()
+        {
+        }
+
+        public void Jump()
+        {
+        }
+
+        public void HauHau()
+        {
+            Console.WriteLine("Hau! Hau!");
+        }
+    }
+}
+
